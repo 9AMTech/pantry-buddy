023e9bebcd483273baca804b43d115baf162bd8b
Ahmad Mughrabi <48345582+9AMTech@users.noreply.github.com>
2021-04-13 20:04:13 -0400

    Updating all DB Files

--- php/removePOST.php
+++ php/removePOST.php
@@ -4,8 +4,16 @@
 	// START OF DATABASE CONNECTION AND GRABBING DATABASE INFO
 			
 			// Connecting to the Database
-			require_once("../db/credentials.php");
-			$connection = new mysqli($host, $user, $pass, $db);
+			//Get Heroku ClearDB connection information
+		$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+		$cleardb_server = $cleardb_url["host"];
+		$cleardb_username = $cleardb_url["user"];
+		$cleardb_password = $cleardb_url["pass"];
+		$cleardb_db = substr($cleardb_url["path"],1);
+		$active_group = 'default';
+		$query_builder = TRUE;
+		// Connect to DB
+		$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 			
 			// Verify the Connection
 			if($connection->connect_error)
